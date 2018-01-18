8360065e63c26a52a2c2a4335b6104fa5a18f3dd
RecieverofMemory <35514270+RecieverofMemory@users.noreply.github.com>
2018-01-17 20:38:41 -0700

    Update checkpoints.cpp
    updating

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -43,10 +43,10 @@ namespace Checkpoints
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1507032424, // * UNIX timestamp of last checkpoint block
+        1516245716, // * UNIX timestamp of last checkpoint block
         0,          // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
-        750.0      // * estimated number of transactions per day after checkpoint
+        576     // * estimated number of transactions per day after checkpoint
     };
 
     static MapCheckpoints mapCheckpointsTestnet =
@@ -55,7 +55,7 @@ namespace Checkpoints
         ;
     static const CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1507032223,
+        1516245716,
         0,
         300
     };
